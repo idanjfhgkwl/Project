9547ba5627729a484f5b47b91be5d0afc78e5cf1
idanjfhgkwl <nevergivein@naver.com>
2020-10-27 11:00:32 +0900

    한국데이터거래소 소비트렌드 코리아 2020

---
+++ kdx2020/package.R
@@ -0,0 +1,15 @@
+# 패키지
+
+install.packages("tidyverse")
+install.packages("readxl")
+install.packages("lubridate")
+install.packages("reshape2")
+install.packages("ggrepel")
+
+library(tidyverse)
+library(readxl)
+library(lubridate)
+library(reshape2)
+library(ggrepel)
+library(ggplot2)
+
